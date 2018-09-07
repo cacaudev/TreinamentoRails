2e26b0702481a4ba8fe31e4fdbc18db1448f9686
cacauabreu2013 <cacauabreu2013@gmail.com>
2018-09-06 21:46:07 -0400

    Atualizado ate aula 62

--- notebook/app/controllers/phones_controller.rb
+++ notebook/app/controllers/phones_controller.rb
@@ -4,7 +4,7 @@ class PhonesController < ApplicationController
   # GET /phones
   # GET /phones.json
   def index
-    @phones = Phone.all
+    @phones = Phone.all.includes(:contact)
   end
 
   # GET /phones/1
